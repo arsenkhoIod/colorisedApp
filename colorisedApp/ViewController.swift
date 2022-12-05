b8bda053ef2490e683578c484bbc337ac2d86f87
arsenkhoIod <arsenykholod@yandex.ru>
2022-12-05 22:43:26 +0300

    result without normal color mixing

--- colorisedApp/ViewController.swift
+++ colorisedApp/ViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var viewWindow: UIView!
+    
+    @IBOutlet var labelRed: UILabel!
+    @IBOutlet var labelGreen: UILabel!
+    @IBOutlet var labelBlue: UILabel!
+    
+    @IBOutlet var sliderRed: UISlider!
+    @IBOutlet var sliderGreen: UISlider!
+    @IBOutlet var sliderBlue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .lightGray
+        viewWindow.backgroundColor = .white
+        viewWindow.layer.cornerRadius = viewWindow.frame.height / 10
+        setupSlider(slider: sliderRed, color: .red)
+        setupSlider(slider: sliderGreen, color: .green)
+        setupSlider(slider: sliderBlue, color: .blue)
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        labelRed.text = String(sliderRed.value)
+        viewWindow.backgroundColor = UIColor(red: 255, green: CGFloat(sliderGreen.value) * 2.55, blue: CGFloat(sliderBlue.value) * 2.55, alpha: 1).withAlphaComponent(CGFloat(sliderRed.value))
+        //let color: UIColor = .red
+        //viewWindow.backgroundColor = viewWindow.backgroundColor?.withAlphaComponent(CGFloat(sliderRed.value))
+    }
+    
+    @IBAction func greenSliderAction() {
+        labelGreen.text = String(sliderGreen.value)
+        viewWindow.backgroundColor = UIColor(red: CGFloat(sliderRed.value) * 2.55, green: 255, blue: CGFloat(sliderBlue.value) * 2.55, alpha: 1).withAlphaComponent(CGFloat(sliderGreen.value))
+        //viewWindow.backgroundColor = viewWindow.backgroundColor?.withAlphaComponent(CGFloat(sliderGreen.value))
+        //viewWindow.backgroundColor = viewWindow.backgroundColor.
+    }
+    
+    @IBAction func blueSliderAction() {
+        labelBlue.text = String(sliderBlue.value)
+        viewWindow.backgroundColor = UIColor(red: CGFloat(sliderRed.value) * 2.55, green: CGFloat(sliderGreen.value) * 2.55, blue: 255, alpha: 1).withAlphaComponent(CGFloat(sliderBlue.value))
+        //viewWindow.backgroundColor = viewWindow.backgroundColor?.withAlphaComponent(CGFloat(sliderBlue.value))
+    }
+    
+    private func setupSlider(slider: UISlider, color: UIColor) {
+        slider.value = 0
+        slider.minimumValue = 0
+        slider.maximumValue = 1
+        slider.minimumTrackTintColor = .white
+        slider.maximumTrackTintColor = color
+    }
 }
 
