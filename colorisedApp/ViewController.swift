f06e7d711def587d39565aa4d115eb37bd1c7355
arsenkhoIod <arsenykholod@yandex.ru>
2022-12-06 23:22:34 +0300

    ...

--- colorisedApp/ViewController.swift
+++ colorisedApp/ViewController.swift
@@ -22,42 +22,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .lightGray
-        viewWindow.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
-                                             green: CGFloat(sliderGreen.value),
-                                             blue: CGFloat(sliderBlue.value),
-                                             alpha: 1)
+        setColor()
         viewWindow.layer.cornerRadius = viewWindow.frame.height / 10
         setupSlider(slider: sliderRed, color: .red)
         setupSlider(slider: sliderGreen, color: .green)
         setupSlider(slider: sliderBlue, color: .blue)
         
-        labelRed.text = String(format: "%.2f", sliderRed.value)
-        labelBlue.text = String(format: "%.2f", sliderBlue.value)
-        labelGreen.text = String(format: "%.2f", sliderGreen.value)
+        setValue(for: labelRed, labelBlue, labelGreen)
     }
     
-    @IBAction func redSliderAction() {
-        labelRed.text = String(format: "%.2f", sliderRed.value)
-        viewWindow.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
-                                             green: CGFloat(sliderGreen.value),
-                                             blue: CGFloat(sliderBlue.value),
-                                             alpha: 1)
-    }
-    
-    @IBAction func greenSliderAction() {
-        labelGreen.text = String(format: "%.2f", sliderGreen.value)
-        viewWindow.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
-                                             green: CGFloat(sliderGreen.value),
-                                             blue: CGFloat(sliderBlue.value),
-                                             alpha: 1)
-    }
-    
-    @IBAction func blueSliderAction() {
-        labelBlue.text = String(format: "%.2f", sliderBlue.value)
-        viewWindow.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
-                                             green: CGFloat(sliderGreen.value),
-                                             blue: CGFloat(sliderBlue.value),
-                                             alpha: 1)
+    @IBAction func sliderAction() {
+        setColor()
+        labelRed.text = string(from: sliderRed)
+        labelBlue.text = string(from: sliderBlue)
+        labelGreen.text = string(from: sliderGreen)
     }
     
     private func setupSlider(slider: UISlider, color: UIColor) {
@@ -66,6 +44,29 @@ class ViewController: UIViewController {
         slider.maximumTrackTintColor = color
     }
     
+    private func setColor() {
+        viewWindow.backgroundColor = UIColor(red: CGFloat(sliderRed.value),
+                                             green: CGFloat(sliderGreen.value),
+                                             blue: CGFloat(sliderBlue.value),
+                                             alpha: 1)
+    }
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case labelRed:
+                labelRed.text = string(from: sliderRed)
+            case labelGreen:
+                labelGreen.text = string(from: sliderGreen)
+            default:
+                labelBlue.text = string(from: sliderBlue)
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
+    }
     
 }
 
